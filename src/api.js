43a29ac6484d94cc9369c1e3792b5101935c2065
clurin <lostikplay@gmail.com>
2025-03-15 22:40:20 +0300

    asd

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ export const geoApiOptions = {
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 }
-
+//
 export const GEO_API_URL = process.env.REACT_APP_GEO_API_URL
 export const WEATHER_API_URL = process.env.REACT_APP_WEATHER_API_URL
 export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
